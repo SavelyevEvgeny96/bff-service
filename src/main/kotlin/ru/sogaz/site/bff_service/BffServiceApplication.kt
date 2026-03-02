154db54e99f18c96d4660e018bccdb71164263b4
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2026-03-02 20:55:36 +0300

    Add Service

---
+++ src/main/kotlin/ru/sogaz/site/bff_service/BffServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.sogaz.site.bff_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffServiceApplication>(*args)
+}
