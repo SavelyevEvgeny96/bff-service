6bbc6f924a27c0ea8dc8bac4cbfdd08cff534eac
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-03-18 13:50:10 +0300

    SITE-2386  правка админ ручки

--- src/main/kotlin/ru/sogaz/site/bff/service/controller/admin/AdminPaymentExceptionController.kt
+++ src/main/kotlin/ru/sogaz/site/bff/service/controller/admin/AdminPaymentExceptionController.kt
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 import ru.sogaz.site.bff.service.dto.request.CreatePaymentExceptionRequest
 import ru.sogaz.site.bff.service.dto.response.CreatePaymentExceptionResponse
 import ru.sogaz.site.bff.service.service.AdminPaymentExceptionServiceImpl
+import ru.sogaz.site.filterStarter.services.RequestInfo.getTraceId
+import ru.sogaz.siter.models.resonses.Response
+import ru.sogaz.siter.models.resonses.getSuccessResponse
 import java.util.UUID
 
 /**
@@ -21,6 +24,10 @@ import java.util.UUID
 class AdminPaymentExceptionController(
     private val service: AdminPaymentExceptionServiceImpl,
 ) {
+    companion object {
+        const val CODE_SUCCESS = 1101543201
+    }
+
     /**
      * Создать исключение: запретить оплату продукту указанным paymentType.
      *
@@ -31,8 +38,12 @@ class AdminPaymentExceptionController(
     fun create(
         @PathVariable productId: UUID,
         @RequestBody @Valid request: CreatePaymentExceptionRequest,
-    ): CreatePaymentExceptionResponse {
+    ): Response<CreatePaymentExceptionResponse> {
         val id = service.createException(productId, request.paymentType.trim())
-        return CreatePaymentExceptionResponse(id.toString())
+        return getSuccessResponse(
+            getTraceId(),
+            CODE_SUCCESS,
+            CreatePaymentExceptionResponse(id.toString())
+        )
     }
 }
--- src/main/kotlin/ru/sogaz/site/bff/service/dto/response/CreatePaymentExceptionResponse.kt
+++ src/main/kotlin/ru/sogaz/site/bff/service/dto/response/CreatePaymentExceptionResponse.kt
@@ -4,6 +4,7 @@ package ru.sogaz.site.bff.service.dto.response
  * Ответ о создании исключения.
  */
 class CreatePaymentExceptionResponse(
+
     /** UUID созданной записи product_payment_exception.id. */
     val exceptionId: String,
 )
