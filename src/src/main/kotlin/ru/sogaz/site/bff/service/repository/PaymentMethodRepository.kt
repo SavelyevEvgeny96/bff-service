208bbd7ac98f127861d8fbc8fe7db92c269ca513
Savelev Evgeny <Savelev.Evgeny.V@sogaz.ru>
2026-07-06 13:05:44 +0300

    TEST правка

--- src/src/main/kotlin/ru/sogaz/site/bff/service/repository/PaymentMethodRepository.kt
+++
@@ -1,9 +0,0 @@
-package ru.sogaz.site.bff.service.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import ru.sogaz.site.bff.service.model.PaymentMethod
-import java.util.UUID
-
-interface PaymentMethodRepository : JpaRepository<PaymentMethod, UUID> {
-    fun findByType(type: String?): PaymentMethod?
-}
